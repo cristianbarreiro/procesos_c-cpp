bae1f21fb90cc15eeadcb653310f3e059f64c235
C. Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-10-10 19:20:01 -0300

    fx

--- parteB/forkjoin-dos.cpp
+++ parteB/forkjoin-dos.cpp
@@ -3,28 +3,32 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main() {
+int main()
+{
     pid_t pid1, pid2, pid3, pid4, pid5;
     //--s1--
     printf("Ejecutando S1(PID %d)\n", getpid());
     sleep(1);
     //--s2--
     pid1 = fork();
-    if(pid1 == 0) {
+    if (pid1 == 0)
+    {
         printf("Ejecutando S2(PID %d)\n", getpid());
         sleep(1);
         return 0;
     }
     //--s3--
     pid2 = fork();
-    if(pid2 == 0) {
+    if (pid2 == 0)
+    {
         printf("Ejecutando S3(PID %d)\n", getpid());
         sleep(1);
         return 0;
     }
     //--s4--
     pid3 = fork();
-    if(pid3 == 0) {
+    if (pid3 == 0)
+    {
         printf("Ejecutando S4(PID %d)\n", getpid());
         sleep(1);
         return 0;
@@ -34,17 +38,19 @@ int main() {
     waitpid(pid2, NULL, 0);
     //--s5--
     pid4 = fork();
-    if(pid4 == 0) {
+    if (pid4 == 0)
+    {
         printf("Ejecutando S5(PID %d)\n", getpid());
         sleep(1);
         return 0;
     }
-  
+
     waitpid(pid3, NULL, 0);
     waitpid(pid4, NULL, 0);
     //--s6--
     pid5 = fork();
-    if(pid5 == 0) {
+    if (pid5 == 0)
+    {
         printf("Ejecutando S6(PID %d)\n", getpid());
         sleep(1);
         return 0;
