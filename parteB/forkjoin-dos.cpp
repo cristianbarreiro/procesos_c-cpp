228ced3c3493d7a2b61199008c6ed11ac7d7105e
C. Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2025-10-12 02:11:16 -0300

    fix: verificar errores de creacion de los procesos hijos

--- parteB/forkjoin-dos.cpp
+++ parteB/forkjoin-dos.cpp
@@ -6,27 +6,43 @@
 int main()
 {
     pid_t pid1, pid2, pid3, pid4, pid5;
+    
     //--s1--
     printf("Ejecutando S1(PID %d)\n", getpid());
     sleep(1);
+    
     //--s2--
     pid1 = fork();
+    if (pid1 < 0) {
+        perror("Error al crear S2");
+        return 1;
+    }
     if (pid1 == 0)
     {
         printf("Ejecutando S2(PID %d)\n", getpid());
         sleep(1);
         return 0;
     }
+    
     //--s3--
     pid2 = fork();
+    if (pid2 < 0) {
+        perror("Error al crear S3");
+        return 1;
+    }
     if (pid2 == 0)
     {
         printf("Ejecutando S3(PID %d)\n", getpid());
         sleep(1);
         return 0;
     }
+    
     //--s4--
     pid3 = fork();
+    if (pid3 < 0) {
+        perror("Error al crear S4");
+        return 1;
+    }
     if (pid3 == 0)
     {
         printf("Ejecutando S4(PID %d)\n", getpid());
@@ -36,8 +52,13 @@ int main()
 
     waitpid(pid1, NULL, 0);
     waitpid(pid2, NULL, 0);
+    
     //--s5--
     pid4 = fork();
+    if (pid4 < 0) {
+        perror("Error al crear S5");
+        return 1;
+    }
     if (pid4 == 0)
     {
         printf("Ejecutando S5(PID %d)\n", getpid());
@@ -47,8 +68,13 @@ int main()
 
     waitpid(pid3, NULL, 0);
     waitpid(pid4, NULL, 0);
+    
     //--s6--
     pid5 = fork();
+    if (pid5 < 0) {
+        perror("Error al crear S6");
+        return 1;
+    }
     if (pid5 == 0)
     {
         printf("Ejecutando S6(PID %d)\n", getpid());
